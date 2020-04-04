d0ed531fd19d3b324850fb591d552d61cfd9fa6a
EvaMaeRey <evangeline.mae@gmail.com>
2020-04-04 13:12:44 -0600

    line breaks preserved

--- R/madlibs.R
+++ R/madlibs.R
@@ -1,49 +1,7 @@
 
-
-
-#' remove aeiou
-#'
-#' @param x a string input
-#'
-#' @return a string object
-#' @export
-#'
-#' @examples
-#' remove_aeiou("Hello, World!")
-remove_aeiou <- function(x){ stringr::str_replace_all(x, "[aeiou]", " \\_ ") }
-
-
-
-#' Drop words to quiz knowledge of a text
-#'
-#' @param x a string input usually a paragraph
-#' @param frac a fraction of words to replace with empty spaces
-#'
-#' @return
-#' @export
-#'
-#' @examples
-#' drop_words(x = "The woods are lovely dark and deep, but I have promises to keep and miles to go before I sleep.", frac = .2)
-drop_words <- function(x, frac = .2) {
-
-  x %>%
-    stringr::str_split(" |\\n") %>%
-    .[[1]] %>%
-    tibble::tibble() %>%
-    dplyr::rename(text = ".") %>%
-    # tidytext::unnest_tokens(output = word, input = text) %>%
-    dplyr::mutate(replace = sample(x = c(T,F), size = dplyr::n(), replace = T, prob = c(frac, 1 - frac))) %>%
-    dplyr::mutate(return = dplyr::case_when(replace ~ "______",
-                              !replace ~ text)) %>%
-    dplyr::pull(return) %>%
-    cat()
-
-}
-
-
 create_wandered_lonely <- function(){
 
-"I wandered lonely as a cloud
+  "I wandered lonely as a cloud
 That floats on high o'er vales and hills,
 When all at once I saw a crowd,
 A host, of golden daffodils;
@@ -70,6 +28,62 @@ They flash upon that inward eye
 Which is the bliss of solitude;
 And then my heart with pleasure fills,
 And dances with the daffodils."
+
+}
+
+
+
+
+#' remove aeiou
+#'
+#' @param x a string input
+#'
+#' @return a string object
+#' @export
+#'
+#' @examples
+#' remove_aeiou("Hello, World!")
+remove_aeiou <- function(x){ stringr::str_replace_all(x, "[aeiou]", " \\_ ") }
+
+
+string_as_table <- function(code){
+
+  code %>%
+    stringr::str_split(pattern = "\n") %>%
+    .[[1]] %>%
+    tibble::tibble(string = .) %>%
+    dplyr::mutate(line = 1:dplyr::n())
+
+}
+
+
+#' Drop words to quiz knowledge of a text
+#'
+#' @param x a string input usually a paragraph
+#' @param frac a fraction of words to replace with empty spaces
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' drop_words(x = "The woods are lovely dark and deep,
+#' but I have promises to keep and miles to go before I sleep.", frac = .2)
+drop_words <- function(x, frac = .2) {
+
+  create_wandered_lonely() %>%
+    string_as_table() %>%
+    dplyr::mutate(token = stringr::str_split(string, " |\\n")) %>%
+    tidyr::unnest() %>%
+    dplyr::mutate(replace = sample(x = c("", "______"), size = dplyr::n(),
+                                   replace = T, prob = c(1 - frac, frac))) %>%
+  dplyr::mutate(return = dplyr::case_when(replace == "______" ~ replace,
+                                          replace == "" ~ token)) %>%
+    dplyr::group_by(line) %>%
+    dplyr::mutate(return = paste(return, collapse = " ")) %>%
+    dplyr::distinct(line, return) %>%
+    dplyr::pull(return) %>%
+    paste0(sep = "\n") %>%
+    cat()
 
 }
 
